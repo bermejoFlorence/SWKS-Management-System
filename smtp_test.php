086097d9bbb45819c1050e42c4c17f9420f2d4d2
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-09-21 16:21:37 +0800

    fixing

--- smtp_test.php
+++ smtp_test.php
@@ -15,23 +15,20 @@ try {
   $mail->Host = 'smtp.gmail.com';
   $mail->SMTPAuth = true;
   $mail->Username = 'joshua.lerin@cbsua.edu.ph';
-  $mail->Password = 'drdjfeavapsxuact'; // app password
+  $mail->Password = 'drdjfeavapsxuact'; // Gmail App Password (walang spaces)
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
   $mail->Port = 587;
+
   $mail->SMTPDebug = 2;
   $mail->Debugoutput = 'html';
 
   $mail->setFrom('joshua.lerin@cbsua.edu.ph', 'SMTP Test');
-  $mail->addAddress('' . 'joshua.lerin@cbsua.edu.ph'); // test to self
+  $mail->addAddress('joshua.lerin@cbsua.edu.ph');
   $mail->Subject = 'SMTP Test';
   $mail->Body = 'This is a test email via Gmail SMTP.';
   $mail->isHTML(true);
 
-  if ($mail->send()) {
-    echo "OK: test mail sent.";
-  } else {
-    echo "send() false: ".$mail->ErrorInfo;
-  }
+  echo $mail->send() ? "OK: test mail sent." : "send() false: ".$mail->ErrorInfo;
 } catch (Exception $e) {
   echo "Exception: ".$e->getMessage();
 }
