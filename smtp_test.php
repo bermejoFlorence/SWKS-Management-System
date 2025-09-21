86b30a93af64884725c4c06d245aa42d63c27cbf
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-09-21 16:25:33 +0800

    try

--- smtp_test.php
+++ smtp_test.php
@@ -2,9 +2,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/phpmailer/src/PHPMailer.php';
-require __DIR__ . '/phpmailer/src/SMTP.php';
-require __DIR__ . '/phpmailer/src/Exception.php';
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer/src/SMTP.php';
+require __DIR__ . '/PHPMailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -12,23 +12,23 @@ use PHPMailer\PHPMailer\Exception;
 $mail = new PHPMailer(true);
 try {
   $mail->isSMTP();
-  $mail->Host = 'smtp.gmail.com';
-  $mail->SMTPAuth = true;
-  $mail->Username = 'joshua.lerin@cbsua.edu.ph';
-  $mail->Password = 'drdjfeavapsxuact'; // Gmail App Password (walang spaces)
+  $mail->Host       = 'smtp.gmail.com';
+  $mail->SMTPAuth   = true;
+  $mail->Username   = 'joshua.lerin@cbsua.edu.ph';
+  $mail->Password   = 'drdjfeavapsxuact'; // Gmail App Password (no spaces)
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-  $mail->Port = 587;
+  $mail->Port       = 587;
 
-  $mail->SMTPDebug = 2;
+  $mail->SMTPDebug  = 2;      // show SMTP convo (remove after testing)
   $mail->Debugoutput = 'html';
 
   $mail->setFrom('joshua.lerin@cbsua.edu.ph', 'SMTP Test');
   $mail->addAddress('joshua.lerin@cbsua.edu.ph');
   $mail->Subject = 'SMTP Test';
-  $mail->Body = 'This is a test email via Gmail SMTP.';
+  $mail->Body    = 'This is a test email via Gmail SMTP.';
   $mail->isHTML(true);
 
-  echo $mail->send() ? "OK: test mail sent." : "send() false: ".$mail->ErrorInfo;
+  echo $mail->send() ? 'OK: test mail sent.' : 'send() false: '.$mail->ErrorInfo;
 } catch (Exception $e) {
-  echo "Exception: ".$e->getMessage();
+  echo 'Exception: '.$e->getMessage();
 }
